d5229d12d6cf2adc7ca94c02280fbfcc6dd30131
Matt Cook <mathew.cook@mastery.net>
2022-12-15 14:31:48 -0600

    creating new function and assigning to variable

--- cards/main.go
+++ cards/main.go
@@ -7,8 +7,13 @@ func main() {
 	// declares and assigns a value to variable card
 	// var card string = "Ace of Spades"
 	// := makes Go, figure out what type of variable we are declaring
-	card := "Ace of Spades"
+	card := createCard()
 	// we can also reassign this variable to another string by --
 	// card = "Any string, notice dropping the :"
 	fmt.Println(card)
+}
+
+// requires you tell it what is should return
+func createCard() string {
+	return "Five of Diamonds"
 }
